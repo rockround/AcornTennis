e07cb83de465ae513e343f1766aa362f4c831bcd
Yuuki Ishimaru <yzi107030@utdallas.edu>
2020-10-30 20:30:10 -0500

    Added time until comes home indicator

--- AcornTennis/Assets/Scripts/Objective/GameRule.cs
+++ AcornTennis/Assets/Scripts/Objective/GameRule.cs
@@ -8,10 +8,10 @@ public class GameRule : MonoBehaviour
 {
     public float totalTime = 120;
     public Slider slider;
-    Vector3 centerField1 = new Vector3(-3.92f,.25f,-8.21f);
-    Vector3 extentField1 = new Vector3(6,.5f,8);
-    Vector3 centerField2 = new Vector3(-3.92f,.25f,8.23f);
-    Vector3 extentField2 = new Vector3(6,.5f,8);
+    Vector3 centerField1 = new Vector3(-3.92f, .25f, -8.21f);
+    Vector3 extentField1 = new Vector3(6, .5f, 8);
+    Vector3 centerField2 = new Vector3(-3.92f, .25f, 8.23f);
+    Vector3 extentField2 = new Vector3(6, .5f, 8);
     public Collider field1, field2;
     //Modified publicly by acorns as they spawn and die
     public List<GameObject> acorns;
@@ -20,16 +20,20 @@ public class GameRule : MonoBehaviour
     internal int field2Acorns;
     public InGameMenuController menuController;
     public TMP_Text team1Acorns, team2Acorns;
+
+    public AudioSource soundOfTime;
+    float startSoundVolume;
     // Start is called before the first frame update
     void Start()
     {
+        startSoundVolume = soundOfTime.volume;
         StartCoroutine(timer(totalTime));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     IEnumerator timer(float timePeriod)
     {
@@ -39,10 +43,12 @@ public class GameRule : MonoBehaviour
         {
             float progress = (Time.time - startTime) / timePeriod;
             slider.value = progress;
+            soundOfTime.volume = startSoundVolume * progress;
             yield return new WaitForSecondsRealtime(.05f);
         }
+        soundOfTime.Stop();
 
-        if(field1Acorns > field2Acorns)
+        if (field1Acorns > field2Acorns)
         {
             menuController.GameEnded(2);
         }
@@ -55,10 +61,10 @@ public class GameRule : MonoBehaviour
     {
         if (other.transform.root.name.Contains("Tree"))
             return;
-        if((other.transform.position - centerField1).magnitude < (other.transform.position - centerField2).magnitude )
+        if ((other.transform.position - centerField1).magnitude < (other.transform.position - centerField2).magnitude)
         {
             field1Acorns++;
-            team1Acorns.text = field1Acorns+ "";
+            team1Acorns.text = field1Acorns + "";
         }
         else
         {
